ee1b5185c2cbc56c7c6f70ae6f0242b1e19631a5
jakeportaro <jakeportaro@gmail.com>
2022-10-21 12:58:01 -0700

    Fixing Get events button

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,8 +19,8 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://mmendell.github.io/chat2/"],
-  javascript_origins: ["https://mmendell.github.io", "http://localhost:3000"],
+  redirect_uris: ["https://jakeportaro.github.io/meet/"],
+  javascript_origins: ["https://jakeportaro.github.io", "http://localhost:3000"],
 };
 const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
@@ -132,7 +132,8 @@ module.exports.getCalendarEvents = (event) => {
     return {
       statusCode: 200,
       headers: {
-        "Access-Control-Allow-Origins": "*",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
       },
       body: JSON.stringify({ events: results.data.items }),
     };
