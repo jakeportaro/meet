48b61e0d8b14e67e219de9d3ed166cdf68be8afe
jakeportaro <jakeportaro@gmail.com>
2022-11-20 15:12:55 -0700

    updated home page look

--- src/App.js
+++ src/App.js
@@ -47,6 +47,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>Meet App</h1>
+        <h4>Search a City to find events!</h4>
         <CitySearch
           locations={this.state.locations}
           updateEvents={this.updateEvents}
